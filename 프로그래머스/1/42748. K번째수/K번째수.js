7503a227c489a77b9d247628d11a39761d9ac448
injaehyun <39895634+injaehyun@users.noreply.github.com>
2023-12-29 09:05:44 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,18 @@
+function solution(array, commands) {
+    var answer = [];
+    
+    for (let i = 0; i < commands.length; i++) {
+      
+        let start = commands[i][0];
+        let end = commands[i][1];
+        let k = commands[i][2];
+        
+ 
+        let sliced = array.slice(start - 1, end).sort((a, b) => a - b);
+        
+       
+        answer.push(sliced[k - 1]);
+    }
+    
+    return answer;
+}
